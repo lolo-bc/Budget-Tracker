fe4a7b38c2608a9f88732a1a7fde768c1097ec2d
lolobc <lauren.n.cameron@gmail.com>
2020-06-30 15:00:42 -0400

    changes to make heroku deploy work

--- server.js
+++ server.js
@@ -15,12 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { useNewUrlParser: true,   useFindAndModify: false});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", { useNewUrlParser: true});
 
 
 // routes
