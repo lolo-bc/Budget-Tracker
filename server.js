43e85d0c4c8e2f231c66a8bb74912d5a9085f3a9
lolobc <lauren.n.cameron@gmail.com>
2020-06-29 22:39:47 -0400

    connect db to heroku deploy

--- server.js
+++ server.js
@@ -15,10 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/budget", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true,   useFindAndModify: false});
+
 
 // routes
 app.use(require("./routes/api.js"));
